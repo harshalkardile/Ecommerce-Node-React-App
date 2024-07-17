460ccccbe0f741e5e908604e31b7c9e231faeb46
harshal <kardileharshal567@gmail.com>
2024-07-17 20:17:52 +0530

    setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element= {<h1> Product Listing Component</h1>} />
+            <Route path="/add" element= {<h1> Product add Component</h1>} />
+            <Route path="/update" element= {<h1> Product update Component</h1>} />
+            <Route path="/logout" element= {<h1> Product logout Component</h1>}/>
+            <Route path="/profile" element= {<h1> profile Component</h1>}/>
+           
+
+          </Routes>
+          <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
